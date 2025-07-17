9200a6b8625853259e9d3d6131a861ed1929b074
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2025-07-17 15:36:31 -0500

    login contra servicios, avancc

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -28,7 +28,7 @@ class WelcomeController < ApplicationController
 
     # Hacer petición a tu API con HTTParty
     begin
-      response = HTTParty.post(
+      access_response = HTTParty.post(
         'http://localhost:8080/api/v1/sign-in', # Reemplaza con tu URL
         body: {
           username: username,
@@ -40,9 +40,25 @@ class WelcomeController < ApplicationController
           'Accept' => 'application/json'
         }
       )
+      file_server_response = HTTParty.post(
+        'http://localhost:8090/api/v1/sign-in', # Reemplaza con tu URL
+        body: {}.to_json,
+        headers: {
+          'Content-Type' => 'application/json',
+          'Accept' => 'application/json',
+          'X-Auth-Trigger' => 'dXNlci1zdGlja3lfc2VjcmV0XzEyMzQ1Njc'
+        }
+      )
       # Manejar la respuesta
-      status_code = response.code
-      response_body = response.body
+      status_code = access_response.code
+      response_body = JSON.parse(access_response.body)
+      file_server_status_code = file_server_response.code
+      file_server_response_body = JSON.parse(file_server_response.body)
+      puts '1 +++++++++++++++++++++++++++'
+      puts file_server_response_body['token']
+      puts '2 +++++++++++++++++++++++++++'
+      response_body['file_token'] = file_server_response_body['token']
+      # response_body[:hola] = 'mundo'
       render json: response_body, status: status_code
     rescue Timeout::Error, Errno::EINVAL, Errno::ECONNRESET, EOFError,
       Net::HTTPBadResponse, Net::HTTPHeaderSyntaxError, Net::ProtocolError => e
