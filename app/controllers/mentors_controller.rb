05a6612f9b91f4a16c99bd3b1fee72b292a347d4
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2025-07-20 23:17:17 -0500

    editar mentor

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -75,6 +75,71 @@ class MentorsController < ApplicationController
     end
   end
 
+  def delete
+    begin
+      @mentor = Mentor.find(params[:id])
+      if @mentor.destroy
+        redirect_to mentors_path, notice: "Mentor eliminado"
+      else
+        redirect_to mentors_path, alert: "No se pudo eliminar"
+      end
+    rescue ActiveRecord::RecordNotFound
+      # Si no se encuentra el registro, redirige a una página 404
+      redirect_to not_found_path
+    rescue => e
+      # Captura cualquier error general
+      Rails.logger.error "Error al eleminar el mentor: #{e.message}"
+      flash.now[:alert] = "Ocurrió un error inesperado: #{e.message}"
+      render :index
+    end
+  end
+
+  def edit
+    begin
+      @mentor = Mentor.find(params[:id])
+      @link = '/mentors'
+      render :edit
+    rescue ActiveRecord::RecordNotFound
+      # Si no se encuentra el registro, redirige a una página 404
+      redirect_to not_found_path
+    rescue => e
+      # Captura cualquier error general
+      Rails.logger.error "Error al editar Mentor: #{e.message}"
+      flash.now[:alert] = "Ocurrió un error inesperado: #{e.message}"
+      render :index
+    end
+  end
+
+  def update
+    @mentor = Mentor.find_by(id: params[:id])
+  
+    if @mentor.nil?
+      redirect_to not_found_path, alert: "Mentor no encontrado"
+      return
+    end
+  
+    code = params[:code]
+    full_name = params[:full_name]
+    charge = params[:charge]
+    email = params[:email]
+    linkedln_url = params[:linkedln_url]
+    image_url = params[:image_url].presence || 'uploads/mentors/default.png'
+  
+    if @mentor.update(
+      code: code,
+      full_name: full_name,
+      charge: charge,
+      email: email,
+      linkedln_url: linkedln_url,
+      image_url: image_url,
+    )
+      redirect_to mentors_path, notice: "Mentor actualizado"
+    else
+      flash.now[:alert] = "No se pudo actualizar"
+      render :edit
+    end
+  end
+
   def new
     @link = '/mentors'
   end
